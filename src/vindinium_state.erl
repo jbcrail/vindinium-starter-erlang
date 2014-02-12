4be57d1cbc46b4b37780002f04c184bc6788f29f
Joseph Crail <jbcrail@gmail.com>
2014-02-11 22:13:29 -0500

    Convert JSON to record instead of proplist.

--- src/vindinium_state.erl
+++ src/vindinium_state.erl
@@ -8,11 +8,56 @@
 -record(board, {size, tiles}).
 -record(position, {x, y}).
 
+to_board(Proplist) ->
+    #board{
+      size=proplists:get_value(<<"size">>, Proplist),
+      tiles=proplists:get_value(<<"tiles">>, Proplist)
+      }.
+
+to_position(Proplist) ->
+    #position{
+      x=proplists:get_value(<<"x">>, Proplist),
+      y=proplists:get_value(<<"y">>, Proplist)
+      }.
+
+to_hero(Proplist) ->
+    #hero{
+      id=proplists:get_value(<<"id">>, Proplist),
+      name=proplists:get_value(<<"name">>, Proplist),
+      user_id=proplists:get_value(<<"userId">>, Proplist),
+      elo=proplists:get_value(<<"elo">>, Proplist),
+      pos=to_position(proplists:get_value(<<"pos">>, Proplist)),
+      life=proplists:get_value(<<"life">>, Proplist),
+      gold=proplists:get_value(<<"gold">>, Proplist),
+      mine_count=proplists:get_value(<<"mineCount">>, Proplist),
+      spawn_pos=to_position(proplists:get_value(<<"spawnPos">>, Proplist)),
+      crashed=proplists:get_value(<<"crashed">>, Proplist)
+      }.
+
+to_game(Proplist) ->
+    #game{
+      id=proplists:get_value(<<"id">>, Proplist),
+      turn=proplists:get_value(<<"turn">>, Proplist),
+      max_turns=proplists:get_value(<<"maxTurns">>, Proplist),
+      heroes=lists:map(fun(P) -> to_hero(P) end, proplists:get_value(<<"heroes">>, Proplist)),
+      board=to_board(proplists:get_value(<<"board">>, Proplist)),
+      finished=proplists:get_value(<<"finished">>, Proplist)
+      }.
+
+to_state(Proplist) ->
+    #state{
+      game=to_game(proplists:get_value(<<"game">>, Proplist)),
+      hero=to_hero(proplists:get_value(<<"hero">>, Proplist)),
+      token=proplists:get_value(<<"token">>, Proplist),
+      view_url=proplists:get_value(<<"viewUrl">>, Proplist),
+      play_url=proplists:get_value(<<"playUrl">>, Proplist)
+      }.
+
 from_json(Json) ->
-    kvc:to_proplist(mochijson2:decode(Json)).
+    to_state(kvc:to_proplist(mochijson2:decode(Json))).
 
 finished(State) ->
-    proplists:get_value(<<"finished">>, proplists:get_value(<<"game">>, State)).
+    State#state.game#game.finished.
 
 play_url(State) ->
     binary_to_list(proplists:get_value(<<"playUrl">>, State)).
