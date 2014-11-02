b2d20283240f070453d0ce642425405c4250e8b0
Joseph Crail <jbcrail@gmail.com>
2014-11-02 01:36:02 -0400

    Improve behavior of greedy bot.

--- src/greedy_bot.erl
+++ src/greedy_bot.erl
@@ -4,4 +4,11 @@
 -export([move/1]).
 
 move(State) ->
-    vindinium_board:nearest_unowned_mine(State).
+    Life = vindinium_state:life(State),
+    if
+        Life =< 25 ->
+          vindinium_board:nearest_tavern(State);
+        true ->
+          Moves = [vindinium_board:nearest_unowned_mine(State), vindinium_board:nearest_unowned_mine(State)],
+          lists:nth(random:uniform(length(Moves)), Moves)
+    end.
